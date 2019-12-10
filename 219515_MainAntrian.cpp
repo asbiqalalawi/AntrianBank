7bf79e681c1bb8154204ebae72eb7a682d0e76f3
DandyAgungPratama <49240414+DandyAgungPratama@users.noreply.github.com>
2019-12-10 23:31:29 +0700

    Add files via upload

---
+++ 219515_MainAntrian.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "Antrian.h"
+#include "Denque.h"
+using namespace std;
+
+int main() {
+	int choose;
+	do{
+		//Tampilan menu
+		cout<< "<< ANTRIAN BANK >>\n";
+		cout<< "Menu\n";
+	    cout<< "1. Tambah Antrian\n";
+		cout<< "2. Kurangi Antrian\n";
+		cout<< "3. Keluar \n";
+		cout<< "Pilihan : "; cin >> choose;
+		cout<< endl;
+		switch (choose){
+		case 1:
+			enqueue();
+			break;
+		case 2:
+			dequeue();
+			break;
+		default:
+			cout << "Pilihan tidak tersedia\n";
+			break;
+		}
+	} while (choose !=3);
+	return 0;
+}
